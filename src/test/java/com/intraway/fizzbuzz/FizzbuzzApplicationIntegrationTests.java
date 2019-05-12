862759e83c50e5bcfd9398c58395444b675dc982
Simon Cardona <simon.cardona@autodesk.com>
2019-05-12 10:39:28 -0300

    Arreglo en tests de integracion

--- src/test/java/com/intraway/fizzbuzz/FizzbuzzApplicationIntegrationTests.java
+++ src/test/java/com/intraway/fizzbuzz/FizzbuzzApplicationIntegrationTests.java
@@ -2,18 +2,18 @@ package com.intraway.fizzbuzz;
 
 import com.intraway.fizzbuzz.entities.FBError;
 import com.intraway.fizzbuzz.entities.ListaNumero;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FizzbuzzApplicationIntegrationTests {
 
